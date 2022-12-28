0fbfb7da42fa7ee2561ca7962a8534256bddcc8d
Raden Maulana Rifa'i Abdullah <maulanarifai114@gmail.com>
2022-12-28 21:29:04 +0700

    Create api for send message with twilio

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MessageInstance } from 'twilio/lib/rest/api/v2010/account/message';
+import { Param } from '@nestjs/common/decorators';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('/message/:number')
+  async sendMessage(@Param('number') number: string): Promise<MessageInstance> {
+    try {
+      const result = await this.appService.twilio.messages.create({
+        body: 'This message is delivered via Twilio',
+        from: 'whatsapp:+14155238886',
+        to: `whatsapp:${number}`,
+      });
+      return result;
+    } catch (error) {
+      console.log(error);
+      return error;
+    }
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import * as twilio from 'twilio';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  get twilio() {
+    const accountSid = process.env.TWILIO_ACCOUNT_SID;
+    const authToken = process.env.TWILIO_AUTH_TOKEN;
+    const client = twilio(accountSid, authToken);
+    return client;
   }
 }
